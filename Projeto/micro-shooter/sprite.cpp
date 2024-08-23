42fb4eef998ef62f47991ea8e07074ba0d8c272f
caduHD4 <cadumarianogarciapereira@gmail.com>
2024-08-23 19:44:48 -0300

    adicionando elementos para carregar os sprites

---
+++ Projeto/micro-shooter/sprite.cpp
@@ -0,0 +1,31 @@
+#include "sprite.hpp"
+#include "SDL_image.h"
+
+Sprite::Sprite(const std::string& filePath, SDL_Renderer* renderer, int frameWidth, int frameHeight, int totalFrames, float frameTime)
+    : frameWidth(frameWidth), frameHeight(frameHeight), totalFrames(totalFrames), frameTime(frameTime), currentFrame(0), currentFrameTime(0.0f) {
+    SDL_Surface* tempSurface = IMG_Load(filePath.c_str());
+    texture = SDL_CreateTextureFromSurface(renderer, tempSurface);
+    SDL_FreeSurface(tempSurface);
+
+    srcRect = { 0, 0, frameWidth, frameHeight };
+    destRect = { 0, 0, frameWidth, frameHeight };
+}
+
+Sprite::~Sprite() {
+    SDL_DestroyTexture(texture);
+}
+
+void Sprite::update(float deltaTime) {
+    currentFrameTime += deltaTime;
+    if (currentFrameTime >= frameTime) {
+        currentFrame = (currentFrame + 1) % totalFrames;
+        srcRect.x = currentFrame * frameWidth;
+        currentFrameTime = 0.0f;
+    }
+}
+
+void Sprite::render(SDL_Renderer* renderer, int x, int y) {
+    destRect.x = x;
+    destRect.y = y;
+    SDL_RenderCopy(renderer, texture, &srcRect, &destRect);
+}
